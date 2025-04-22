364b553b478eee3c439e585bc0b9b6c4836214e9
niko <nikoagustio22@gmail.com>
2025-04-22 18:47:22 +0700

    make the pagination

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,15 +1,5 @@
 import { useEffect, useState } from "react";
-import monas from '../assets/Monas.png'
-const indonesiaProvinces = [
-  { province: "Bali", hotels: 1200 },
-  { province: "Jakarta", hotels: 1500 },
-  { province: "West Java", hotels: 800 },
-  { province: "East Java", hotels: 950 },
-  { province: "Yogyakarta", hotels: 600 },
-  { province: "North Sumatra", hotels: 350 },
-  { province: "South Sulawesi", hotels: 450 },
-  { province: "Central Java", hotels: 750 },
-];
+import monas from "../assets/Monas.png";
 
 export default function Card({ data }) {
   return (
@@ -19,11 +9,7 @@ export default function Card({ data }) {
           className="relative h-60 rounded-xl overflow-hidden shadow-md w-full bg-pink"
           key={index}
         >
-          <img
-              src={monas}
-              className="w-full object-cover"
-              alt="Monas"
-            />
+          <img src={monas} className="object-contain" alt="Monas" />
           <div className="absolute bottom-2 left-2 right-2 text-black text-sm font-semibold flex justify-between ">
             <p>{item.province}</p>
             <p>{item.hotels}</p>
--- client/src/components/Pagination.jsx
+++ client/src/components/Pagination.jsx
@@ -1,71 +1,30 @@
 import { useState } from "react";
-const indonesiaProvinces = [
-  { province: "Bali", hotels: 1200 },
-  { province: "Jakarta", hotels: 1500 },
-  { province: "West Java", hotels: 800 },
-  { province: "East Java", hotels: 950 },
-  { province: "Yogyakarta", hotels: 600 },
-  { province: "North Sumatra", hotels: 350 },
-  { province: "South Sulawesi", hotels: 450 },
-  { province: "Central Java", hotels: 750 },
-  { province: "Aceh", hotels: 200 },
-  { province: "Lampung", hotels: 400 },
-  { province: "Banten", hotels: 300 },
-  { province: "North Sulawesi", hotels: 250 },
-  { province: "South Sumatra", hotels: 500 },
-  { province: "West Sumatra", hotels: 400 },
-  { province: "East Kalimantan", hotels: 250 },
-  { province: "West Kalimantan", hotels: 300 },
-  { province: "Central Kalimantan", hotels: 200 },
-  { province: "North Kalimantan", hotels: 150 },
-  { province: "Maluku", hotels: 120 },
-  { province: "North Maluku", hotels: 100 },
-  { province: "Papua", hotels: 180 },
-  { province: "West Papua", hotels: 150 },
-  { province: "Gorontalo", hotels: 90 },
-  { province: "Sulawesi Tenggara", hotels: 200 },
-  { province: "Sulawesi Tengah", hotels: 150 },
-  { province: "Nusa Tenggara Barat", hotels: 500 },
-  { province: "Nusa Tenggara Timur", hotels: 350 },
-  { province: "Jambi", hotels: 180 },
-  { province: "Riau", hotels: 250 },
-  { province: "Bengkulu", hotels: 150 },
-  { province: "Kepulauan Riau", hotels: 200 },
-  { province: "Kalimantan Selatan", hotels: 250 },
-  { province: "Kalimantan Tengah", hotels: 200 },
-  { province: "Kalimantan Barat", hotels: 250 },
-];
+import Card from "./Card";
 
-export default function Pagination() {
+export default function Pagination({data}) {
   const [currentPage, setCurrentPage] = useState(1);
 
   const itemPerPage = 8;
   const startIndex = (currentPage - 1) * itemPerPage;
   const endIndex = startIndex + itemPerPage;
-  const item = indonesiaProvinces.slice(startIndex, endIndex);
+  const item = data.slice(startIndex, endIndex);
 
   return (
     <div>
-      <ul>
-        {item.map((item, index) => (
-          <li>
-            <p>
-              {item.province}
-              {index}
-            </p>
-          </li>
-        ))}
-      </ul>
-      <div className="w-10 flex justify-between">
+      <div className="grid grid-cols-4 gap-5">
+
+      <Card data={item}/>
+      </div>
+      <div className="w-full flex justify-between">
         <button
-          className="bg-pink p-5 font-semibold  rounded-full"
+          className="text-2xl p-5 font-semibold  rounded-full"
           onClick={() => setCurrentPage(currentPage - 1)}
         >
           {"<"}
         </button>
-        <p className="bg-pink p-5 font-semibold  rounded-full">{currentPage}</p>
+        <p className="p-5 font-semibold  rounded-full">{currentPage}</p>
         <button
-          className="bg-pink p-5 font-semibold rounded-full"
+          className="text-2xl p-5 font-semibold rounded-full"
           onClick={() => setCurrentPage(currentPage + 1)}
         >
           {">"}
