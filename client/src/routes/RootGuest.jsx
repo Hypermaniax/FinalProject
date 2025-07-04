957de4c1d75651ceef300eba8ae3d46f671a6232
niko <nikoagustio22@gmail.com>
2025-07-04 21:55:00 +0700

    FIX THE ROUTING AN MAKE MORE SIMPLE

--- client/src/routes/RootGuest.jsx
+++ client/src/routes/RootGuest.jsx
@@ -9,6 +9,12 @@ import BookingDetails from "../pages/guest/BookingDetails";
 import { ToastContainer } from "react-toastify";
 import AuthModalGuest from "../pages/guest/AuthModalGuest";
 import ProtectedRoute from "./protected/ProtectRoute";
+import DashboardGuest from "../components/DashboardGuest";
+import Wishlist from "../components/WishList";
+import MyTransaction from "../components/MyTransaction";
+import AccountSetting from "../components/AccountSetting";
+import RequestToBook from "../components/RequestTobook";
+
 
 export default function RootGuest() {
   return (
@@ -21,13 +27,21 @@ export default function RootGuest() {
           <Route path="space" element={<Spaces />} />
           <Route path="space/:id" element={<BookingDetails />} />
           <Route
-            path="bookings/*"
+            path="bookings"
             element={
-              <ProtectedRoute role={"guest"}>
+              <ProtectedRoute role='guest'>
                 <Bookings />
               </ProtectedRoute>
             }
-          />
+          >
+            <Route index element={<DashboardGuest />} />
+            <Route path="bookings" element={<DashboardGuest />} />
+            <Route path="wishlist" element={<Wishlist />} />
+            <Route path="transaction" element={<MyTransaction />} />
+            <Route path="account" element={<AccountSetting />} />
+            <Route path="logout" element={<>logout</>} />
+            <Route path="payment-bookings/:id" element={<RequestToBook />} />
+          </Route>
           <Route path="support" element={<Support />} />
           <Route path="login-guest" element={<AuthModalGuest />} />
         </Routes>
